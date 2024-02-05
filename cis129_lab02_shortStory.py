b8e45f2c5cefbebb6585b8e56002881ab7ead941
TheRodster80 <159051093+TheRodster80@users.noreply.github.com>
2024-02-05 10:08:12 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,5 @@
-print('Hello, World!')
+# Author: Jarrod Saffioti
+# CIS129 Lab 2
+# Prints a short story
+
+print("Once upon a time, there was a wee lad named \"Calerb.\"\nHe was a very beautiful man.\nHe took a programming class with his brother, Jarrod.\nThe end.")
