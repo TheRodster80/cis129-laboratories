e67cc5760d6a09f1edeb52353453b36326d5604d
TheRodster80 <159051093+TheRodster80@users.noreply.github.com>
2024-02-21 10:11:24 -0700

    Add files via upload

---
+++ CIS129_jsaffioti_Lab4.py
@@ -0,0 +1,111 @@
+# Module 4 Lab-4
+# Jarrod Saffioti
+# 02/20/2024
+# This program calculates bonuses based on the performance of the store and its employees.
+
+# declare local variables
+monthlySales = 0  # monthly sales amount
+storeAmount = 0  # store bonus amount
+empAmount = 0  # employee bonus amount
+salesIncrease = 0  # percent of sales increase
+prompt = 'How many monthly sales have you had? ' # prompt will be a string literal
+
+# This code gets the monthly sales
+
+monthlySales = float(input(prompt))
+
+# This code determines the store bonus
+
+if monthlySales >= 110000:
+     storeAmount = 6000
+elif monthlySales >= 100000:
+     storeAmount = 5000
+elif monthlySales >= 90000:
+     storeAmount = 4000
+elif monthlySales >= 80000:
+     storeAmount = 3000
+else:
+     storeAmount = 0
+
+# This code gets the percent of increase in sales
+salesIncrease = float(input('What is the percent increase in sales? '))
+salesIncrease = salesIncrease / 100
+
+# This code determines the employee bonus
+if salesIncrease >= .05:
+     empAmount = 75
+elif salesIncrease >= .04:
+     empAmount = 50
+elif salesIncrease >= .03:
+     empAmount = 40
+else:
+     empAmount = 0
+
+# This code prints the bonus information
+print("The store bonus amount is $", storeAmount)
+print("The employee bonus amount is $", empAmount)
+if (storeAmount == 6000) and (empAmount == 75):
+     print('Congrats! You have reached the highest bonus amounts possible! ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''# fig02_01.py
+"""Comparing integers using if statements and comparison operators."""
+
+print('Enter two integers, and I will tell you',
+      'the relationships they satisfy.')
+
+# read first integer
+number1 = int(input('Enter first integer: '))
+
+# read second integer
+number2 = int(input('Enter second integer: '))
+
+if number1 == number2:
+     print(number1, 'is equal to', number2)
+else:
+     print(number1, 'is not equal to', number2)
+
+if number1 < number2:
+     print(number1, 'is less than', number2)
+else:
+     print(number1, 'is greater than', number2)
+
+if number1 <= number2:
+     print(number1, 'is less than or equal to', number2)
+else:
+     print(number1, 'is greater than or equal to', number2)'''
